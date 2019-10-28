c1e34c619eab6f530001e483c2b04bfc7768a192
NOCTLJRNE <baoan1602@gmail.com>
2019-10-28 12:48:32 +0100

    edit rpcConfig_sample

--- rpcConfig_sample.js
+++ rpcConfig_sample.js
@@ -1,8 +1,8 @@
 export default {
   rpcHost: "", // 127.0.0.1 if you run the app & bakcground daemon are on the same host
-  rpcPort: "", // 127.0.0.1 16348 for Morty
+  rpcPort: "", // 16348 for Morty
   rpcUser: "", //your_rpc_user_name
   rpcPassword: "", //your_rpc_user_password
   rpcPubkey: "", //your pubkey, if no pubkey is provided, the pubkey used to launch the daemon is the default
-  rpcRaddress: "" //your Raddress corresponding to the pubkey
+  rpcRaddress: "" //your Raddress corresponding to the pubkey, will be caculated from your pubkey, so leave it blank
 };
